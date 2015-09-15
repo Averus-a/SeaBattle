e1a0fdb4fed7371248bee07a7efb20837fe5ea42
Artem Saikin <art.saikin@gmail.com>
2015-09-15 22:02:59 +0300

    Ended main camera control work; PlayerController class cleared of debug; Smooth movement implemented;

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -12,10 +12,15 @@ public class PlayerController : MonoBehaviour {
 	float deltaX,deltaY,deltaScale;
 	float cachedX,cachedY,cachedScale,cachedTime;
 	public float speed;
+	bool isRunning = false;
+
+	//To pass instance to StartCoroutine not directly
+	Coroutine smoothHelper;
 
 	// Use this for initialization
 	void Start () {
 		speed = 100f;
+		smoothHelper = null;
 	}
 	
 	// Update is called once per frame
@@ -28,36 +33,50 @@ public class PlayerController : MonoBehaviour {
 			cachedX += deltaX;
 			cachedY += deltaY;
 			cachedScale += deltaScale;
+			
+			//Button down time
 			if (isAnyKeyDown()) {
-				cachedTime += Time.deltaTime;
-			Debug.Log(cachedTime);
+					cachedTime += Time.deltaTime;
 			}
+			
 			OverviewCamera.Translate (deltaX,deltaY,deltaScale);
-			if (isStatic ()) {
-				StartCoroutine (SmoothMove (deltaX, deltaY, deltaScale, cachedTime));
-				cachedX = cachedY = cachedScale = cachedTime = 0;
+			
+			if (isRunning && isAnyKeyDown ()) {
+			//Debug.Log ("STOP COROUTINE RIGHT THERE");
+			StopCoroutine (smoothHelper);
 		}
-	}
+			if (isCameraControlEnded()) {
+				smoothHelper = StartCoroutine (SmoothMove (deltaX, deltaY, deltaScale, 2*cachedTime));
+				cachedX = cachedY = cachedScale = cachedTime = 0;
+
+			}
+		}
+
 
 	//smooth coroutine
-	//last for 2 seconds(50 tics)
-	//total smooth moving is impact from pressing button divide by 5;
-	//divider count from arithmetical progression N*(N+1)/2, N=tics;
+	//f/Timing - decresing multiplier for impacts
 	IEnumerator SmoothMove(float impactX,float impactY, float impactScale,float Timing) {
-		float f;
-		Debug.Log ("Time is " + Timing);
-		for ( f = Timing*50f; f >0; f -= 1f){
-				OverviewCamera.Translate (impactX*f/10,impactY*f/10,impactScale*f/10);
+		float f, m;
+		//Debug.Log ("Time is " + Timing/2);
+		float frameLength = Time.deltaTime;
+		//100: smooth time = move time x2
+		//float timeTicsCount = Timing * 100f;
 
-				//Debug
-				//Debug.Log("X:"+ impactX/divider*f*Timing);
-				yield return new WaitForSeconds(0.02f); //so slow. Need to find other solution;
+		isRunning = true;
+		for ( f = Timing; f >0; f -= Time.deltaTime){
+			frameLength = Time.deltaTime;
+			m = f/Timing;
+			OverviewCamera.Translate (impactX*m,impactY*m,impactScale*m);
+
+				yield return null;
 			}
+		isRunning = false;
+		//Debug.Log ("ENDED");
 		}
-	public static bool isStatic(){
+	public static bool isCameraControlEnded(){
 		return Input.GetButtonUp ("Horizontal") || Input.GetButtonUp ("Vertical") || Input.GetButtonUp ("Mouse ScrollWheel");
 	}
 	public static bool isAnyKeyDown(){
-		return Input.GetButton("Horizontal") || Input.GetButton("Vertical") || Input.GetButton("Mouse ScrollWheel");
+		return Input.GetButton ("Horizontal") || Input.GetButton ("Vertical") || Input.GetButton ("Mouse ScrollWheel");			
 	}
 }
