77f32a2f3461951ba2ef5c77fc1a99bcfaf1b396
Artem Saikin <art.saikin@gmail.com>
2015-09-17 13:28:04 +0300

    refactored PlayerController class. Check algorithm performance.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -9,8 +9,8 @@ public class PlayerController : MonoBehaviour {
 	//for Sensetivity scrolls = 1
 	public float scaleSensitivity = 5;
 
-	float deltaX,deltaY,deltaScale;
-	float cachedX,cachedY,cachedScale,cachedTime;
+	float deltaX, deltaY, deltaScale;
+	float cachedTime, cachedScroll;
 	public float speed;
 	bool isRunning = false;
 
@@ -19,51 +19,48 @@ public class PlayerController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		speed = 100f;
 		smoothHelper = null;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-			//Reading Player input
-			deltaX = Input.GetAxis ("Horizontal") * Time.deltaTime*speed;
-			deltaY = Input.GetAxis ("Vertical") * Time.deltaTime*speed;
-			deltaScale = Input.GetAxis ("Mouse ScrollWheel") * Time.deltaTime*speed*scaleSensitivity;
-			cachedX += deltaX;
-			cachedY += deltaY;
-			cachedScale += deltaScale;
-			
-			//Button down time
-			if (isAnyKeyDown()) {
-					cachedTime += Time.deltaTime;
+		//Reading Player input and any key down;
+		//Button down time;
+		//Test delta in condition block or in Update (directly) count performance;
+
+		ReadAxeInput (ref deltaX, ref deltaY,ref deltaScale);
+
+		if(isAnyInputed()) {
+					cachedTime += Time.deltaTime;					
+					OverviewCamera.Translate (deltaX,deltaY,deltaScale);
+					Debug.Log ("Moved to "+ deltaX +" "+deltaY+" "+deltaScale);
 			}
 			
-			OverviewCamera.Translate (deltaX,deltaY,deltaScale);
-			
-			if (isRunning && isAnyKeyDown ()) {
-			//Debug.Log ("STOP COROUTINE RIGHT THERE");
-			StopCoroutine (smoothHelper);
+			if (isRunning && isAnyInputed()) {
+				Debug.Log ("STOP COROUTINE RIGHT THERE");
+				StopCoroutine (smoothHelper);
 		}
 			if (isCameraControlEnded()) {
 				smoothHelper = StartCoroutine (SmoothMove (deltaX, deltaY, deltaScale, 2*cachedTime));
-				cachedX = cachedY = cachedScale = cachedTime = 0;
-
+				cachedTime = 0;
 			}
-		}
+		cachedScroll = deltaScale;
+	}
 
 
 	//smooth coroutine
 	//f/Timing - decresing multiplier for impacts
 	IEnumerator SmoothMove(float impactX,float impactY, float impactScale,float Timing) {
-		float f, m;
-		//Debug.Log ("Time is " + Timing/2);
-		float frameLength = Time.deltaTime;
-		//100: smooth time = move time x2
-		//float timeTicsCount = Timing * 100f;
 
+		Debug.Log ("Scroll impact is " + impactScale);
+
+		float f, m;
+		float frameLength = Time.deltaTime;
 		isRunning = true;
-		for ( f = Timing; f >0; f -= Time.deltaTime){
+
+		//better use Math.SmoothDamp ?
+		for ( f = Timing; f >0; f -= frameLength){
 			frameLength = Time.deltaTime;
 			m = f/Timing;
 			OverviewCamera.Translate (impactX*m,impactY*m,impactScale*m);
@@ -71,12 +68,31 @@ public class PlayerController : MonoBehaviour {
 				yield return null;
 			}
 		isRunning = false;
+
 		//Debug.Log ("ENDED");
-		}
-	public static bool isCameraControlEnded(){
-		return Input.GetButtonUp ("Horizontal") || Input.GetButtonUp ("Vertical") || Input.GetButtonUp ("Mouse ScrollWheel");
 	}
-	public static bool isAnyKeyDown(){
-		return Input.GetButton ("Horizontal") || Input.GetButton ("Vertical") || Input.GetButton ("Mouse ScrollWheel");			
+
+	public bool isCameraControlEnded(){
+
+		return Input.GetButtonUp ("Horizontal") || Input.GetButtonUp ("Vertical") || ScrollEnded ();
+	
+	}
+
+	public bool isAnyInputed(){
+
+		return deltaX!=0 || deltaY!=0 || deltaScale!=0;	
+
+	}
+	private bool ScrollEnded(){
+
+		if ((Input.GetAxis ("Mouse ScrollWheel") == 0) && (cachedScroll != 0))
+			return true;
+		return false;
+	}
+	public void ReadAxeInput(ref float x, ref float y, ref float z){
+
+		x = Input.GetAxis ("Horizontal") * Time.deltaTime*speed;
+		y = Input.GetAxis ("Vertical") * Time.deltaTime*speed;
+		z = Input.GetAxis ("Mouse ScrollWheel") * Time.deltaTime*speed*scaleSensitivity;	
 	}
 }
